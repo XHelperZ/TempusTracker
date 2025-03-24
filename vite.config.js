fd493a47ef29cb5e0c0ec762e00299e5ee09cce9
Finn <finnpan2@gmail.com>
2025-03-24 12:51:41 +0100

    Test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TempusTracker",
+  base: "https://xhelperz.github.io/TempusTracker/",
 })
